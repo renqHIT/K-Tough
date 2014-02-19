db413338be616ccccad37f4b2535ea0e3325c776
renqHIT <renq654321@gmail.com>
2014-02-19 18:48:57 +0800

    修改：      toughness.py

--- py/toughness.py
+++ py/toughness.py
@@ -3,9 +3,10 @@ from numpy.random import random
 from numpy.random import poisson
 from copy import deepcopy
 import sys
+import pdb
 
-f_handler = open('debug.log','w')
-sys.stdout = f_handler
+#f_handler = open('debug.log','w')
+#sys.stdout = f_handler
 
 def toughness(g):
 	toughness = float("inf")
@@ -22,7 +23,7 @@ def toughness(g):
 				residual.remove_vertex(residual.vertex(x))
 				print 'residual graph : ', residual
 				# w is the number of connected components of G-S
-				w = num_cc(residual)
+				w = num_cc2(residual)
 				print 'w(G-S) = %d' % w
 				# k is the temp value of toughness
 				k = i / (w * 1.0)
@@ -54,26 +55,53 @@ def combinations(iterable, r):
         yield tuple(pool[i] for i in indices)
 
 
-visited = {}
+#visited = {}	
 
-def explore(g, v):
+def explore(g, v, visited):
 	visited[v] = True
-	for u in v.out_neighbours():
+	for u in v.all_neighbours():
+		#pdb.set_trace()
 		if not visited[u]:
-			explore(g, u)
+			explore(g, u, visited)
 
 
 def num_cc(g):
 	cc = 0
+	visited = {}	
 	for v in g.vertices():
+		print 'v = ', v
 		visited[v] = False
 
 	for v in g.vertices():
 		if not visited[v]:
 			cc = cc + 1
-			explore(g, v)
+			explore(g, v, visited)
 
 	return cc
+
+
+def num_cc2(g):
+	cc = 1
+	comp, hist = label_components(g)
+	print 'comp.a = ', comp.a
+	print 'max of comp.a = ', max(comp.a)
+	print 'hist = ', hist
+	print 'len(hist)', len(hist)
+	return cc + max(comp.a)
+
+
+def num_cc3(g):
+	seen = set()
+	def component(node):
+		nodes = set([node])
+		while nodes:
+			node = nodes.pop()
+			seen.add(node)
+			nodes |= neighbors[node] - seen
+			yield node
+		for node in neighbors:
+			if node not in seen:
+				yield component(node)	
 
 
 if __name__ == '__main__':
@@ -82,7 +110,7 @@ if __name__ == '__main__':
 	#g = load_graph("./data/running-examples/running-example-1.xml.gz")
 	g = load_graph("./data/running-examples/path.xml.gz")
 	#g = load_graph("./data/running-examples/circle.xml.gz")
-	g.set_directed(False)
+	#g.set_directed(False)
 
 	# calculate toughness
 	t = toughness(g)
