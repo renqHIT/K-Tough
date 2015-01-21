b4606f4ed595fe0b034950c4ef856467cd968f0c
renqHIT <renq654321@gmail.com>
2015-01-21 19:52:18 +0800

    deleted:    RUN.md 	new file:   cluster-submit.sh 	new file:   log.out 	modified:   src/main/scala/toughness.scala

---
+++ cluster-submit.sh
@@ -0,0 +1,1 @@
+spark-submit --class "Toughness" --master spark://cluster01:7077 target/scala-2.10/toughness_2.10-1.0.jar
--- src/main/scala/toughness.scala
+++ src/main/scala/toughness.scala
@@ -8,7 +8,7 @@ import org.apache.spark.rdd.RDD
 
 object Toughness{
   def main(args:Array[String]){
-    val conf = new SparkConf().setMaster("spark://cluster07:7077").setAppName("Toughness").set("spark.executor.memory", "6g")
+    val conf = new SparkConf().setMaster("spark://cluster01:7077").setAppName("Toughness").set("spark.executor.memory", "6g")
     val sc = new SparkContext(conf)
     val graph:Graph[Int, Int] = GraphLoader.edgeListFile(sc, "/home/hadoop/data/roadNet-CA.txt")
     val inDegrees = graph.inDegrees
@@ -24,7 +24,9 @@ object Toughness{
     //vertice5.collect.foreach(println(_))
     println("[INFO] graph.degrees type is: " + degrees.getClass)
     val degree10 = degrees.filter{ case (d : (VertexId,Int)) => d._2 >= 10 }
-    degree10.collect.foreach(println(_))
+    //degree10.collect.foreach(println(_))
+    //val debugString = vertices.toDebugString
+    //println(debugString)
     //degree10.collect.foreach(println(_))
     //2.compute toughness for vertices above
     //3.learn the relation between vertex degree and toughness, using, say linear regression
