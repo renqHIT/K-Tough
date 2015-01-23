7bbcb33c62bff4f2c4b982543b6ebacff38b9ba9
renqHIT <renq654321@gmail.com>
2015-01-23 13:27:23 +0800

    modified test suite 	modified:   src/test/scala/ShouldSuite.scala

--- src/test/scala/ShouldSuite.scala
+++ src/test/scala/ShouldSuite.scala
@@ -12,11 +12,36 @@ class ToughTest extends FlatSpec with ShouldMatchers{
     it should "return 1/2 when calculating a line graph" in 
     {
       //val line = new graph()
-      //toughness(line) should be 0.5
+      //toughness(line) should be (0.5)
     }
     it should "return 1 when calculating a circle graph" in
     {
       //val circle = new graph()
-      //toughness(circle) should be 1
+      //toughness(circle) should be (1)
+    }
+    it should "return infinity when calculating a complete graph" in 
+    {
+      //val complete = new graph()
+      //toughness(complete) should be infinity
+    }
+    it should "return 0.25 with running example 1" in
+    {
+    }
+    it should "return 0.5 with running example 2" in 
+    {
+    }
+
+  "The toughness finder" should
+    "return a list of subgraphs with toughness greater than t" in
+    {
+      //val t = 0.5
+      //val data = new graph()
+      //val subgraphs = finder(data, t)
+    }
+    it should "return an empty list when having no subgraphs with toughess greater than t" in 
+    {
+      //val t = 100.0
+      //val data = new graph()
+      //val subgraphs = finder(data, t)
     }
 }
