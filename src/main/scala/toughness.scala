2f01415a8cefe0ce21880da3dcc009e3ada32bfa
renqHIT <renq654321@gmail.com>
2015-01-26 16:23:29 +0800

    modified test in local mode, add rmat graph 	modified:   src/main/scala/toughness.scala 	modified:   src/test/scala/ShouldSuite.scala

--- src/main/scala/toughness.scala
+++ src/main/scala/toughness.scala
@@ -8,28 +8,31 @@ import org.apache.spark.rdd.RDD
 
 object Toughness{
   def main(args:Array[String]){
-    val dataDir = "hdfs://usr/renq/k-tough/data"
-    val conf = new SparkConf().setMaster("spark://cluster01:7077").setAppName("Toughness").set("spark.executor.memory", "6g")
-      val sc = new SparkContext(conf)
-      val graph:Graph[Int, Int] = GraphLoader.edgeListFile(sc, s"$dataDir/roadNet-CA.txt")
-      val inDegrees = graph.inDegrees
-      val degrees : VertexRDD[Int] = graph.degrees
-      val vertices = graph.vertices
-      val edges = graph.edges
-      val triplets = graph.triplets
-      //save partitions of graph into local file system
-      //vertices.saveAsTextFile("/home/hadoop/partition/default-partitions")
-      //1.find vertices with degree higher than 10
-      val vertice5 = vertices.filter{ case (d : (VertexId, Int)) => d._1 >= 1970000}
-      println("Vertices with ID higher than 1970000: " + vertice5.count())
-      //vertice5.collect.foreach(println(_))
-      println("graph.degrees type is: " + degrees.getClass)
-      val degree10 = degrees.filter{ case (d : (VertexId,Int)) => d._2 >= 10 }
-      //degree10.collect.foreach(println(_))
-      
-      //println(debugString)
-      //degree10.collect.foreach(println(_))
-      //2.compute toughness for vertices above
-      //3.learn the relation between vertex degree and toughness, using, say linear regression
+    //val dataDir = "hdfs://cluster01:9090/usr/renq/k-tough/data"
+    //val dataDir = "hdfs://usr/renq/k-tough/data"
+    val dataDir = "/home/renq/k-tough/data"
+    //val conf = new SparkConf().setMaster("spark://cluster01:7077").setAppName("Toughness").set("spark.executor.memory", "6g")
+    //val sc = new SparkContext(conf)
+    val sc = new SparkContext("local", "Toughness", "127.0.0.1", List("target/scala-2.10/toughness_2.10-1.0.jar"))
+    val graph:Graph[Int, Int] = GraphLoader.edgeListFile(sc, s"$dataDir/roadNet-CA.txt")
+    val inDegrees = graph.inDegrees
+    val degrees : VertexRDD[Int] = graph.degrees
+    val vertices = graph.vertices
+    val edges = graph.edges
+    val triplets = graph.triplets
+    //save partitions of graph into local file system
+    //vertices.saveAsTextFile("/home/hadoop/partition/default-partitions")
+    //1.find vertices with degree higher than 10
+    val vertice5 = vertices.filter{ case (d : (VertexId, Int)) => d._1 >= 1970000}
+    println("Vertices with ID higher than 1970000: " + vertice5.count())
+    //vertice5.collect.foreach(println(_))
+    println("graph.degrees type is: " + degrees.getClass)
+    val degree10 = degrees.filter{ case (d : (VertexId,Int)) => d._2 >= 10 }
+    //degree10.collect.foreach(println(_))
+
+    //println(debugString)
+    //degree10.collect.foreach(println(_))
+    //2.compute toughness for vertices above
+    //3.learn the relation between vertex degree and toughness, using, say linear regression
   }
 }
--- src/test/scala/ShouldSuite.scala
+++ src/test/scala/ShouldSuite.scala
@@ -1,57 +1,88 @@
-import org.scalatest.FunSpec import org.scalatest.FlatSpec
+import org.scalatest.FunSpec 
+import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
 import org.apache.spark._
 import org.apache.spark.graphx._
+import org.apache.spark.graphx.util.GraphGenerators
 import org.apache.spark.rdd.RDD
 
 class ToughTest extends FlatSpec with ShouldMatchers{
-  val dataDir = "hdfs://usr/renq/k-tough/data"
-  val conf = new SparkConf().setMaster("spark://cluster01:7077").setAppName("Toughness Test").set("spark.executor.memory", "6g")
-    val sc = new SparkContext(conf)
+  //val dataDir = "hdfs://cluster01:9090/usr/renq/k-tough/data"
+  val dataDir = "/home/renq/k-tough/data/"
+  //val conf = new SparkConf().setMaster("spark://cluster01:7077").setAppName("Toughness Test").set("spark.executor.memory", "6g")
+  //val sc = new SparkContext(conf)
+  val sc = new SparkContext("local", "Toughness", "127.0.0.1", List("target/scala-2.10/toughness_2.10-1.0.jar"))
   "The toughness calculator" should
-    "return 1/(n-1) when calculating a star graph" in
-    {
-      val star = GraphLoader.edgeListFile(sc, s"$dataDir/star.txt")
-      //toughness(star) should be 
-    }
-    it should "return 1/2 when calculating a line graph" in 
-    {
-      val line = GraphLoader.edgeListFile(sc, s"$dataDir/line.txt")
-      //toughness(line) should be (0.5)
-    }
-    it should "return 1 when calculating a circle graph" in
-    {
-      val circle = GraphLoader.edgeListFile(sc, s"$dataDir/circle.txt")
-      //toughness(circle) should be (1)
-    }
-    it should "return infinity when calculating a complete graph" in 
-    {
-      val complete = GraphLoader.edgeListFile(sc, s"$dataDir/complete.txt")
-      //toughness(complete) should be infinity
-    }
-    it should "return 0.25 with running example 1" in
-    {
-      val eg1 = GraphLoader.edgeListFile(sc, s"$dataDir/eg1.txt")
-      //toughness(eg1) should be 0.25
-    }
-    it should "return 0.5 with running example 2" in 
-    {
-      val eg2 = GraphLoader.edgeListFile(sc, s"$dataDir/eg2.txt")
-      //toughness(eg2) should be 0.5
-    }
-    it should "give reasonable toughness with roadNet-CA" in
-    {
-      val road = GraphLoader.edgeListFile(sc, s"$dataDir/roadNet-CA.txt")
-      //toughness(road) should be ???
-    {
-      //val t = 0.5
-      //val data = new graph()
-      //val subgraphs = finder(data, t)
-    }
-    it should "return an empty list when having no subgraphs with toughess greater than t" in 
-    {
-      //val t = 100.0
-      //val data = new graph()
-      //val subgraphs = finder(data, t)
-    }
+  "return 1/(n-1) when calculating a star graph" in
+  {
+    //val star = GraphLoader.edgeListFile(sc, s"$dataDir/star.txt")
+    val star = GraphGenerators.starGraph(sc, 6)
+    //toughness(star) should be 
+  }
+  it should "return 1/2 when calculating a line graph" in 
+  {
+    val line = GraphLoader.edgeListFile(sc, s"$dataDir/line.txt")
+    //val line = Graph(sc.parallelize(List((0L,"a"), (1L, "b"), (2L, "c"))), 
+    //    sc.parallelize(List(Edge(0L, 1L, 1), Edge(0L, 2L, 1), 2)))
+    //toughness(line) should be (0.5)
+  }
+  it should "return 1 when calculating a circle graph" in
+  {
+    val circle = GraphLoader.edgeListFile(sc, s"$dataDir/circle.txt")
+    //toughness(circle) should be (1)
+  }
+  it should "return infinity when calculating a complete graph" in 
+  {
+    val complete = GraphLoader.edgeListFile(sc, s"$dataDir/complete.txt")
+    //toughness(complete) should be infinity
+  }
+  it should "return 0.25 with running example 1" in
+  {
+    val eg1 = GraphLoader.edgeListFile(sc, s"$dataDir/eg1.txt")
+    //toughness(eg1) should be 0.25
+  }
+  it should "return 0.5 with running example 2" in 
+  {
+    val eg2 = GraphLoader.edgeListFile(sc, s"$dataDir/eg2.txt")
+    //toughness(eg2) should be 0.5
+  }
+  it should "give reasonable toughness with roadNet-CA" in
+  {
+    val road = GraphLoader.edgeListFile(sc, s"$dataDir/roadNet-CA.txt")
+    //toughness(road) should be ???
+  }
+  it should "give reasonable toughness with lognormal graph" in
+  {
+    //val lognormal = GraphGenerators.logNormalGraph(sc, numVertices = 100, numEParts = 0, mu = 4.0, sigma = 1.3)
+    //toughness(lognormal) should be ???
+  }
+  it should "give reasonable toughness with RMAT graph" in 
+  {
+    val rmat = GraphGenerators.rmatGraph(sc, 10, 50)
+    //toughness(rmat) should be ???
+  }
+
+  "The toughness finder" should 
+  "return a list of subgraphs with toughness less than 0.5" in 
+  {
+    //val t = 0.5
+    //val data = new graph()
+    //val subgraphs = finder(data, t)
+  }
+  it should "return an empty list when having no subgraphs with toughess greater than t" in 
+  {
+    //val t = 100.0
+    //val data = new graph()
+    //val subgraphs = finder(data, t)
+  }
+  it should "perform well on lognormal graph" in
+  {
+    //val lognormal = GraphGenerators.logNormalGraph(sc, numVertices = 100, numEParts = 0, mu = 4.0, sigma = 1.3)
+    //toughness(lognormal) should be ???
+  }
+  it should "perform well on RMAT graph" in 
+  {
+    val rmat = GraphGenerators.rmatGraph(sc, 10, 50)
+    //toughness(rmat) should be ???
+  }
 }
