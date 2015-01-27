4c77480fd5f9f6c0bf9ba58322a5c1035aee7311
renqHIT <renq654321@gmail.com>
2015-01-27 22:23:34 +0800

    modified:   src/main/scala/toughness.scala 	deleted:    src/test/scala/.SynthBenchmark.scala.swp 	modified:   src/test/scala/ShouldSuite.scala

--- src/test/scala/ShouldSuite.scala
+++ src/test/scala/ShouldSuite.scala
@@ -1,3 +1,5 @@
+package org.hit.renq.toughness
+
 import org.scalatest.FunSpec 
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
@@ -8,28 +10,37 @@ import org.apache.spark.rdd.RDD
 
 class ToughTest extends FlatSpec with ShouldMatchers{
   //val dataDir = "hdfs://cluster01:9090/usr/renq/k-tough/data"
-  val dataDir = "/home/renq/k-tough/data/"
+  val dataDir = "/home/renq/k-tough/data"
   //val conf = new SparkConf().setMaster("spark://cluster01:7077").setAppName("Toughness Test").set("spark.executor.memory", "6g")
   //val sc = new SparkContext(conf)
   val sc = new SparkContext("local", "Toughness", "127.0.0.1", List("target/scala-2.10/toughness_2.10-1.0.jar"))
+  val calculator = new Toughness
   "The toughness calculator" should
   "return 1/(n-1) when calculating a star graph" in
   {
     //val star = GraphLoader.edgeListFile(sc, s"$dataDir/star.txt")
-    val star = GraphGenerators.starGraph(sc, 6)
-    //toughness(star) should be 
+    //val star = GraphGenerators.starGraph(sc, 6)
+    //val n = 5
+    //val star = GraphGenerators.starGraph(sc, n)
+    //val nodes = sc.parallelize(List(0L, 1L, 2L, 3L, 4L, 5L))
+    //val edges = sc.parallelize(List(Edge(0L,1L,1), Edge(0L,2L,1), Edge(0L,3L,1), Edge(0L,4L,1), Edge(0L,5L,1), 5))
+    //val star = Graph(nodes, edges)
+    val star = Graph(
+      sc.parallelize(List((0L,1), (1L,1), (2L,1), (3L,1), (4L,1), (5L,1))),
+    sc.parallelize(List(Edge(0L,1L,1), Edge(0L,2L,1), Edge(0L,3L,1), Edge(0L,4L,1), Edge(0L,5L,1)), 5))
+    calculator.toughness(star) should be (0.2)
   }
   it should "return 1/2 when calculating a line graph" in 
   {
     val line = GraphLoader.edgeListFile(sc, s"$dataDir/line.txt")
     //val line = Graph(sc.parallelize(List((0L,"a"), (1L, "b"), (2L, "c"))), 
     //    sc.parallelize(List(Edge(0L, 1L, 1), Edge(0L, 2L, 1), 2)))
-    //toughness(line) should be (0.5)
+    //calculator.toughness(line) should be (0.5)
   }
   it should "return 1 when calculating a circle graph" in
   {
     val circle = GraphLoader.edgeListFile(sc, s"$dataDir/circle.txt")
-    //toughness(circle) should be (1)
+    //calculator.toughness(circle) should be (1)
   }
   it should "return infinity when calculating a complete graph" in 
   {
