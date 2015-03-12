2b25d2ab93df5c18d22436b5f6ed3b3f68c4402a
renqHIT <renq654321@gmail.com>
2015-03-12 18:15:38 +0800

    modified:   .gitignore 	deleted:    src/main/scala/.toughness.scala.swp 	modified:   src/main/scala/toughness.scala

--- src/main/scala/toughness.scala
+++ src/main/scala/toughness.scala
@@ -1,5 +1,3 @@
-package org.hit.renq.toughness
-
 import org.apache.spark.SparkContext  
 import org.apache.spark.SparkContext._  
 import org.apache.spark.SparkConf  
@@ -9,7 +7,7 @@ import org.apache.spark.graphx._
 import org.apache.spark.graphx.GraphOps
 import org.apache.spark.rdd.RDD 
 
-class Toughness{
+object Toughness{
   def main(args:Array[String]){
     //val dataDir = "hdfs://cluster01:9090/usr/renq/k-tough/data"
     //val dataDir = "hdfs://usr/renq/k-tough/data"
@@ -18,70 +16,50 @@ class Toughness{
     //val sc = new SparkContext(conf)
     val sc = new SparkContext("local", "Toughness", "127.0.0.1", List("target/scala-2.10/toughness_2.10-1.0.jar"))
     val road:Graph[Int, Int] = GraphLoader.edgeListFile(sc, s"$dataDir/roadNet-CA.txt")
-    val eg1 = GraphLoader.edgeListFile(sc, s"$dataDir/eg1.txt")
-    val combine_eg1:List[List[VertexId]] = eg1.vertices.collect().toList.map(kv => kv._1).toSet[VertexId].subsets.map(_.toList).toList
-    var seperator:collection.mutable.Set[VertexId] = collection.mutable.Set()
-    var tough = 1.0
-    eg1.numVertices match {
-      case x if x < 32 => {
-        //seperator = combine_eg1.toSet()
-        for(cs <- combine_eg1){
-          for(cx <- cs){
-            seperator = collection.mutable.Set(cx)
+    val testCases = List("eg1", "eg2", "eg3", "circle", "line", "star", "complete")
+    for (test <-testCases){
+      println(s"Running Graph: $test")
+      var tough = 1.0
+      val eg = GraphLoader.edgeListFile(sc, s"$dataDir/$test.txt")
+      //println("Graph size" + eg.connectedComponents.vertices.collect().toList.size)
+      val combine_eg:List[List[VertexId]] = eg.vertices.collect().toList.map(kv => kv._1).toSet[VertexId].subsets.map(_.toList).toList
+      var seperator:collection.mutable.Set[VertexId] = collection.mutable.Set()
+      val sTime = System.nanoTime
+      eg.numVertices match {
+        case x if x < 32 => {
+          tough = 1.0
+          for(cs <- combine_eg){
+            seperator = collection.mutable.Set()
+            for(cx <- cs){
+              seperator += cx
+            }
+            val sub_eg = eg.subgraph(vpred = (vid, attr) => !(seperator contains vid))
+            val ccl = sub_eg.connectedComponents.vertices.collect().toList.map(t=>t._2) 
+            val cc = ccl.toSet.size
+            //println("seperator: "+seperator)
+            //println("cc: %d".format(cc))
+            if(seperator.size != 0 && cc != 0){
+              tough = math.min(tough, seperator.size.toFloat/cc)
+            }
           }
-          val sub_eg = eg1.subgraph(vpred = (vid, attr) => seperator contains vid)
-          val cc = sub_eg.connectedComponents.vertices.collect().toList.size
-          tough = math.min(tough, seperator.size.toFloat/cc)
+          tough
         }
-        tough
-      }
-      case x if x > 32 => {
-        seperator = collection.mutable.Set()
-        for(i <- 1 to 4){
-          seperator += eg1.pickRandomVertex()
-          val sub_eg = eg1.subgraph(vpred = (vid, attr) => seperator contains vid)
-          val cc = sub_eg.connectedComponents.vertices.collect().toList.size
-          tough = seperator.size.toFloat/cc
+        case x if x > 32 => {
+          seperator = collection.mutable.Set()
+          for(i <- 1 to 4){
+            seperator += eg.pickRandomVertex()
+            val sub_eg = eg.subgraph(vpred = (vid, attr) => seperator contains vid)
+            val cc = sub_eg.connectedComponents.vertices.collect().toList.size
+            tough = seperator.size.toFloat/cc
+          }
         }
+        case _ => Nil
       }
-      case _ => Nil
-    }
-    tough
-    //println(s"Toughness of Graph $graph is $tough")
-    //val project_eg = eg1.mask(sub_eg)
-    //val neighbors = GraphOps.collectNeighborIds()
-    //val inDegrees = graph.inDegrees
-    //val degrees : VertexRDD[Int] = graph.degrees
-    //val vertices = graph.vertices
-    //val edges = graph.edges
-    //val triplets = graph.triplets
-    //2.compute toughness for vertices above
-    //3.learn the relation between vertex degree and toughness, using, say linear regression
-  }
-
-  /*
-   * Compute the toughness of a graph
-   *
-   * @param graph the graph filename to compute toughness
-   *
-   * @return exact toughness when graph size below 32 nodes, 
-   *    approximate toughness when graph size above 32 nodes
-   */
-  def toughness(graph: Graph[Int,Int]) : Float = {
-    val sc = new SparkContext("local", "Toughness", "127.0.0.1",
-      List("target/scala-2.10/toughness_2.10-1.0.jar"))
-    val combine_eg1 = graph.vertices.collect().toList.map(kv => kv._1)
-      .toSet[VertexId].subsets.map(_.toList).toList
-      val seperator:collection.mutable.Set[VertexId] = collection.mutable.Set()
-      for(i <- 1 to 4){
-        seperator += graph.pickRandomVertex()
-      }
-      val subgraph = graph.subgraph(vpred = (vid, attr) => seperator contains vid)
-      val ccsize = subgraph.connectedComponents.vertices.collect().toList.size
-      val tough = seperator.size.toFloat/ccsize
-      println(s"Toughness of Graph $graph is $tough")
+      val eTime = System.nanoTime
+      val micros = (eTime - sTime)/1000
+      println("%d microseconds".format(micros))
+      println(s"Toughness of $test: %f".format(tough))
       tough
+    }
   }
 }
-
-
