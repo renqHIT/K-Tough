03d33f6f332e71518921a034723def2afb85f6a2
renqHIT <renq654321@gmail.com>
2015-04-17 14:36:05 +0800

    new file:   data/eg3.txt 	deleted:    src/main/scala/.operators.scala.swp 	modified:   src/main/scala/operators.scala

--- src/main/scala/operators.scala
+++ src/main/scala/operators.scala
@@ -1,5 +1,6 @@
 import org.apache.spark.SparkContext._
 import org.apache.spark.graphx.PartitionStrategy
+import org.apache.spark.graphx.PartitionStrategy._
 import org.apache.spark.graphx.util.GraphGenerators
 import org.apache.spark.{SparkContext, SparkConf}
 import scala.collection.mutable.Map
@@ -10,7 +11,7 @@ import java.io.{PrintWriter, FileOutputStream}
 
 object Operator{
   def main(args:Array[String]){
-    val partitionNames = List("RandomVertexCut", "EdgePartition1D", "EdgePartition2D", "CanonicalRandomVertexCut")
+    val partitionNames:List[PartitionStrategy] = List(RandomVertexCut, EdgePartition1D, EdgePartition2D, CanonicalRandomVertexCut)
     var niter = 10
     var numVertices = 100
     var numEPart: Option[Int] = None
@@ -27,6 +28,19 @@ object Operator{
 
     // Repartition the graph
     val graph = partitionStrategy.foldLeft(unpartitionedGraph)(_.partitionBy(_)).cache()
+    val e1d = graph.partitionBy(EdgePartition1D, 4)
+    val e2d = graph.partitionBy(EdgePartition2D, 4)
+    val canonical = graph.partitionBy(CanonicalRandomVertexCut, 4)
+    val rvert = graph.partitionBy(RandomVertexCut, 4)
+
+    for(pa <- partitionNames){
+      println(s"Partition by $pa")
+      var pstart = System.currentTimeMillis()
+      graph.partitionBy(pa, 4).collectNeighbors(EdgeDirection.Either)
+      val partitionTime = System.currentTimeMillis - pstart
+      println(s"Partition time = ${partitionTime/1000.0} seconds")
+    }
+
     var startTime = System.currentTimeMillis()
     val numEdges = graph.edges.count()
     println(s"Done creating graph. Num Vertices = $numVertices, Num Edges = $numEdges")
