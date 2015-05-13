02d563b6a80e4066114828ec5d5ed682359e9a80
Qiang <renq654321@gmail.com>
2015-05-13 21:53:17 +0800

    new file:   sampling.scala modi

--- src/main/scala/partition.scala
+++ src/main/scala/partition.scala
@@ -1,22 +1,26 @@
-import java.io.{File, PrintWriter, FileNotFoundException}
-
+import java.io.{File, FileNotFoundException}
 import org.apache.commons.io.FileUtils
-
 import scala.io.Source
-import org.apache.spark.graphx.{PartitionID, VertexId, PartitionStrategy}
+
 
 object Partitions{
   def main (args: Array[String]) {
     //val dataDir = "/home/renq/k-tough/data"
     //val filename = "data/roadNet-CA.txt"
-    val filename = "data/eg1.txt"
+    val filename = "data/roadNet-CA.txt"
     val numParts = 4
-    val output: Array[File] = new Array[File](numParts)
+    val numStrategies = 4
+    val output: Array[File] = new Array[File](numParts * numStrategies)
 
+    //EdgePartition2D
+    println("Start Partitioning: EdgePartition2D")
+    for( i <- 0 to numParts-1 ) {
+      output(i) = new File(s"data/partitions/EdgePartition2D/part-$i.txt")
+    }
     try {
       for (line <- Source.fromFile(filename).getLines()) {
         if(line.startsWith("#")){
-          println(line)
+          //println(line)
         }else {
           val src: String = line.split("\\s+")(0)
           val dst: String = line.split("\\s+")(1)
@@ -25,7 +29,7 @@ object Partitions{
           val col = (math.abs(src.toInt * mixingPrime) % ceilSqrtNumParts).toInt
           val row = (math.abs(dst.toInt * mixingPrime) % ceilSqrtNumParts).toInt
           val partitionTo = (col * ceilSqrtNumParts + row) % numParts
-          println(src, dst, partitionTo)
+          //println(src, dst, partitionTo)
           FileUtils.writeStringToFile(output(partitionTo), src+", "+dst+'\n', true)
         }
       }
@@ -34,54 +38,74 @@ object Partitions{
       case ex: ArrayIndexOutOfBoundsException => println("ArrayIndexOutOfBoundsException")
     }
 
-    /*
-    case object EdgePartition2D {
-      override def getPartition(src: VertexId, dst: VertexId): PartitionID = {
-        val ceilSqrtNumParts: PartitionID = math.ceil(math.sqrt(numParts)).toInt
-        val mixingPrime: VertexId = 1125899906842597L
-        val col: PartitionID = (math.abs(src * mixingPrime) % ceilSqrtNumParts).toInt
-        val row: PartitionID = (math.abs(dst * mixingPrime) % ceilSqrtNumParts).toInt
-        (col * ceilSqrtNumParts + row) % numParts
-      }
+    //EdgePartition1D
+    println("Start Partitioning: EdgePartition1D")
+    for( i <- 0 to numParts-1 ) {
+      output(i + 4) = new File(s"data/partitions/EdgePartition1D/part-$i.txt")
     }
-
-    /**
-     * Assigns edges to partitions using only the source vertex ID, colocating edges with the same
-     * source.
-     */
-    case object EdgePartition1D{
-      override def getPartition(src: VertexId, dst: VertexId): PartitionID = {
-        val mixingPrime: VertexId = 1125899906842597L
-        (math.abs(src * mixingPrime) % numParts).toInt
-      }
-    }
-
-
-    /**
-     * Assigns edges to partitions by hashing the source and destination vertex IDs, resulting in a
-     * random vertex cut that colocates all same-direction edges between two vertices.
-     */
-    case object RandomVertexCut {
-      override def getPartition(src: VertexId, dst: VertexId): PartitionID = {
-        math.abs((src, dst).hashCode()) % numParts
-      }
-    }
-
-
-    /**
-     * Assigns edges to partitions by hashing the source and destination vertex IDs in a canonical
-     * direction, resulting in a random vertex cut that colocates all edges between two vertices,
-     * regardless of direction.
-     */
-    case object CanonicalRandomVertexCut{
-      override def getPartition(src: VertexId, dst: VertexId): PartitionID = {
-        if (src < dst) {
-          math.abs((src, dst).hashCode()) % numParts
-        } else {
-          math.abs((dst, src).hashCode()) % numParts
+    try {
+      for (line <- Source.fromFile(filename).getLines()) {
+        if(line.startsWith("#")){
+          //println(line)
+        }else {
+          val src: String = line.split("\\s+")(0)
+          val dst: String = line.split("\\s+")(1)
+          val mixingPrime: Long = 1125899906842597L
+          val partitionTo = (math.abs(src.toInt * mixingPrime) % numParts).toInt
+          FileUtils.writeStringToFile(output(partitionTo), src+", "+dst+'\n', true)
         }
       }
+    } catch {
+      case ex: FileNotFoundException => println("FileNotFoundException.")
+      case ex: ArrayIndexOutOfBoundsException => println("ArrayIndexOutOfBoundsException")
     }
-    */
+
+    //RandomVertexCut
+    println("Start Partitioning: RandomVertexCut")
+    for( i <- 0 to numParts-1 ) {
+      output(i + 8) = new File(s"data/partitions/RandomVertexCut/part-$i.txt")
+    }
+    try {
+      for (line <- Source.fromFile(filename).getLines()) {
+        if(line.startsWith("#")){
+          //println(line)
+        }else {
+          val src: String = line.split("\\s+")(0)
+          val dst: String = line.split("\\s+")(1)
+          val partitionTo = math.abs((src.toInt, dst.toInt).hashCode()) % numParts
+          FileUtils.writeStringToFile(output(partitionTo), src+", "+dst+'\n', true)
+        }
+      }
+    } catch {
+      case ex: FileNotFoundException => println("FileNotFoundException.")
+      case ex: ArrayIndexOutOfBoundsException => println("ArrayIndexOutOfBoundsException")
+    }
+
+    //CanonicalRandomVertexCut
+    println("Start Partitioning: CanonicalRandomVertexCut")
+    for( i <- 0 to numParts-1 ) {
+      output(i + 12) = new File(s"data/partitions/CanonicalRandomVertexCut/part-$i.txt")
+    }
+    try {
+      for (line <- Source.fromFile(filename).getLines()) {
+        if(line.startsWith("#")){
+          //println(line)
+        }else {
+          val src: String = line.split("\\s+")(0)
+          val dst: String = line.split("\\s+")(1)
+          if (src < dst) {
+            val partitionTo = math.abs((src.toInt, dst.toInt).hashCode()) % numParts
+            FileUtils.writeStringToFile(output(partitionTo), src+", "+dst+'\n', true)
+          } else {
+            val partitionTo = math.abs((dst.toInt, src.toInt).hashCode()) % numParts
+            FileUtils.writeStringToFile(output(partitionTo), src+", "+dst+'\n', true)
+          }
+        }
+      }
+    } catch {
+      case ex: FileNotFoundException => println("FileNotFoundException.")
+      case ex: ArrayIndexOutOfBoundsException => println("ArrayIndexOutOfBoundsException")
+    }
+    println("Finish! ")
   }
 }
